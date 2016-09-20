e6aef08e19e1cb66c7287c9bc8455cfda89365c9
Abhishek Lekshmanan <abhishek@suse.com>
2016-09-20 11:29:32 +0200

    initial commit
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

---
+++ aws.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+    Name = "terraform-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.0.0/24"
+  tags {
+    Name = "terraform-public-subnet"
+  }
+  depends_on = ["aws_internet_gateway.default"]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_security_group" "default_vpc" {
+  name = "all-vpc-traffic"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    self = true
+  }
+
+  egress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    self = true
+  }
+}
+
+resource "aws_security_group" "ssh_http" {
+  name = "ssh"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "internet" {
+  name = "internet"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "salt-master" {
+  ami = "ami-b7b4fedd"
+  instance_type = "t2.micro"
+  tags {
+    Name = "terraform-test"
+  }
+  subnet_id = "${aws_subnet.public.id}"
+  security_groups = ["${aws_security_group.default_vpc.id}", "${aws_security_group.ssh_http.id}"]
+  key_name = "abhishekl"
+  depends_on = ["aws_internet_gateway.default"]
+  tags = {
+    Name = "salt-master"
+  }
+  source_dest_check = false
+}
+
+output "master-ip" {
+  value = "${aws_instance.salt-master.public-ip}"
+}
